eed9fdbf75412e1575ac6387841fb943afb71633
Gary Doublé <garydouble@live.com>
2016-08-04 22:49:58 +0100

    super basic routing and crude 404

--- src/slaer.js
+++ src/slaer.js
@@ -1,79 +1,143 @@
-/*!
-The heart and sole of the entire system.
-*/
+// The heart and sole of the entire system.
 function SlaerApp(parent, path) {
-    this._parent = parent;
-    this._segments = SlaerApp.extractSegments(path);
+	this._parent = parent;
+	this._children = [];
+	this._routes = {};
+    this._path = path;
+	
+	if (parent) {
+		this._parent._children.push(this);
+	}
 };
 
-/*!
-The regex pattern used to identify individual path segments.
-*/
+
+// The regex pattern used to identify individual path segments.
 SlaerApp.SEGMENT_EXTRACTION_PATTERN = /[^\/]+/g;
 
-/*!
-Extracts the segment values from the given path.
-*/
+
+// Extracts the segment values from the given path.
 SlaerApp.extractSegments = function(path) {
     path = path || '';
 
     var segments = [];
 
-    // We only use replace because it gives us a convenient pattern for auto-looping the values.
+    // We use replace because it gives us a convenient easy pattern for auto-extracting the values.
     path.replace(SlaerApp.SEGMENT_EXTRACTION_PATTERN, function (match) { segments.push(match); });
 
     return segments;
 }
 
-/*!
-Creates a new slaer app issolation at the given path.
-*/
-SlaerApp.prototype.app = function(path) {
-    return new SlaerApp(this, path);
+// Defines a new route.
+SlaerApp.prototype.route = function(path, handler) {
+	var segments = SlaerApp.extractSegments(path);
+	
+	var route = this;
+	
+	for (var s in segments) {
+		route = route._routes[segments[s]] = route._routes[segments[s]] || {
+			_routes: {}
+		};
+	}
+	
+	route._handler = handler;
 };
 
-/*!
-Returns the application path segments. 
-*/ 
-SlaerApp.prototype.segments = function() {
-    return this._parent ? this._parent.segments() + this._segments : this._segments; 
-};
 
-/*!
-Returns the application path.
-*/
-SlaerApp.prototype.path = function() {
-    return '/' + this.segments().join('/');
-};
-
-/*!
-Executes the given path relative to this aplication's path.
-*/
+// Executes the given path relative to this aplication's path.
 SlaerApp.prototype.execute = function(path) {
-    console.log("executing:", path, SlaerApp.extractSegments(path));
+	var segments = SlaerApp.extractSegments(path);
+	
+	var route = this;
+	
+	for (var s in segments) {
+		route = route._routes[segments[s]];
+		
+		if (!route) {
+			throw new Error("404: Path '" + path + "' was not found!");
+		}
+	}
+	
+	if (typeof route._handler !== 'function') {
+		throw new Error("404: Path '" + path + "' was not found!");
+	}
+	
+	route._handler.call();
 }
 
 
-// Bootstrap the root application.
+// Navigates to the given path.
+SlaerApp.prototype.go = function(path) {
+	window.location = '#' + path;
+};
+
+
+// Bootstrap the default application.
 var slaer = new SlaerApp();
 
-// Include a reference to the SlaerApp class for testing.
-slaer.__SlaerApp = SlaerApp;
+slaer.defaultPath = '/';
 
+// Check for a browser.
+// TODO: Integrate proper module exposure
 if (typeof window !== 'undefined') {
-    window.addEventListener('hashchange', function() {
-        slaer.execute(window.location.hash.substring(1));
+    window.addEventListener('hashchange', function() {		
+		if (window.location.hash === '#' || window.location.hash === '#/') {
+			if (slaer.defaultPath && slaer.defaultPath === '/') {
+				slaer.execute(slaer.defaultPath);
+			} else {
+				slaer.go(slaer.defaultPath);
+			}
+		} else {
+			slaer.execute(window.location.hash.substring(1));
+		}
     });
 
     window.addEventListener('load', function() {
         if (!window.location.hash) {
-            window.location = '#' + window.location.pathname;
+			if (window.location.pathname === '/') {
+				slaer.go(slaer.defaultPath);
+			} else {
+				slaer.go(window.location.pathname);
+			}
         } else {
-            slaer.execute(window.location.hash.substring(1));
+			if (window.location.hash === '#' || window.location.hash === '#/') {
+				slaer.go(slaer.defaultPath);
+			} else {
+				slaer.execute(window.location.hash.substring(1));
+			}
         }
     });
 }
 
+// Not a browser so assume NodeJS (for now)
 else if (typeof module !== 'undefined' && module.exports) {
+
+	// Include a special reference to the SlaerApp class for testing.
+	slaer.__SlaerApp = SlaerApp;
+
+	// Export the module
     module.exports = slaer;
 }
+
+
+
+
+
+
+
+
+
+
+
+/*!
+Creates a new slaer app issolation at the given path.
+*/
+// SlaerApp.prototype.app = function(path) {
+    // if (!path || path.trim() === '/') {
+        // throw new Error("app path cannot be empty or a root '/'")
+    // }
+
+    // return new SlaerApp(this, path);
+//};
+
+
+
